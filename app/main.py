da26e1b4d972c1cb3b939f77f4a452ca08a06bf1
zhrssh <98146850+zhrssh@users.noreply.github.com>
2023-12-19 17:35:35 +0800

    Minor fixes on fetching IPs

--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
 """ Python application for handling retrieval of public IPv4 and 
     IPv6 addresses using public API.
 """
-
 from requests import get
 
 
 def get_ipv4():
-    """ Function for retreiving IPv4 address
+    """Function for retreiving IPv4 address
 
-        returns:
-            ipv4['ip']: Public IP address of host
-            
-       """
-    ipv4 = get('https://ipapi.co/json/').json() # use request library to get json from ipapi.co
-    return ipv4['ip']
+    returns:
+        ipv4['ip']: Public IP address of host
+
+    """
+    ipv4 = get(
+        "https://api.ipify.org?format=json"
+    ).json()  # use request library to get json from ipapi.co
+    return ipv4["ip"]
 
 
 def get_ipv6():
-    """Function for retreiving IPv6 address"""
-    response = requests.get("https://api64.ipify.org?format=json")
-    ip = response.json()["ip"]
-    return ip
+    """Function for retreiving IPv6 address
+
+    returns:
+        ipv6["ip"]: Public IPv6 address of host
+    """
+    ipv6 = get("https://api64.ipify.org?format=json").json()
+    return ipv6["ip"]
 
 
 """ Driver function """
